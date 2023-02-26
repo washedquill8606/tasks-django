6afd64b9a64d8eadf7c463f71dcd903ee94eebdd
Alex Puga <scdo117ds@outlook.com>
2023-02-26 15:34:38 -0600

    login and logout auth

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.models import User
-from django.http import HttpResponse
+from django.db import IntegrityError
 
 # Create your views here.
 
@@ -26,7 +26,7 @@ def signup(request):
                 user.save()
                 login(request, user)
                 return redirect('tasks')
-            except:
+            except IntegrityError:
                 return render(request, 'signup.html', {
                     'form': UserCreationForm,
                     'error': 'Username already exists',
@@ -39,3 +39,26 @@ def signup(request):
 
 def tasks(request):
     return render(request, 'tasks.html')
+
+
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html', {
+            'form': AuthenticationForm
+        })
+    else:
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'signin.html', {
+                'form': AuthenticationForm,
+                'error' : 'Username or password is incorrect'
+            })
+        else:
+            login(request, user)
+            return redirect('tasks')
